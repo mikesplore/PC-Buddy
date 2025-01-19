2a552f565609d7c85fb0cae94b7f9c0edf17b722
mikesplore <mikepremium8@gmail.com>
2025-01-19 23:27:03 +0300

    Initial Commit

--- app/src/main/java/com/mike/vendor/model/users/UserRepository.kt
+++ app/src/main/java/com/mike/vendor/model/users/UserRepository.kt
@@ -3,41 +3,67 @@ package com.mike.vendor.model.users
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 
-class UserRepository(private val userDao: UserDao) {
+
+class UserRepository(
+    private val userDao: UserDao,
+
+) {
+    private val firebaseDataSource=  FirebaseUserDataSource()
 
     fun insertUser(user: User): Flow<Result<String>> = flow {
         withContext(Dispatchers.IO) {
             userDao.insertUser(user)
         }
-        emit(Result.success("User inserted successfully"))
+        firebaseDataSource.insertUser(user).collect { result ->
+            emit(result)
+        }
     }.catch { exception ->
         emit(Result.failure(exception))
     }
 
-    fun getAllUsers(): Flow<Result<List<User>>> =
-        userDao.getAllUsers()
-            .map { Result.success(it) }
-            .catch { exception ->
-                emit(Result.failure(exception))
+    fun getAllUsers(): Flow<Result<List<User>>> = flow {
+        firebaseDataSource.getAllUsers().collect { result ->
+            if (result.isSuccess) {
+                result.getOrNull()?.let { users ->
+                    withContext(Dispatchers.IO) {
+                        users.forEach {
+                            userDao.insertUser(it)
+                        }
+                    }
+                }
             }
+            emit(result)
+        }
+    }.catch { exception ->
+        emit(Result.failure(exception))
+    }
 
-
-    fun getUserById(id: String): Flow<Result<User>> =
-        userDao.getUserById(id)
-            .map { Result.success(it) }
-            .catch { exception ->
-                emit(Result.failure(exception))
+    fun getUserById(id: String): Flow<Result<User>> = flow {
+        firebaseDataSource.getUserById(id).collect { result ->
+            if (result.isSuccess) {
+                result.getOrNull()?.let { user ->
+                    withContext(Dispatchers.IO) {
+                        userDao.insertUser(user)
+                    }
+                }
             }
+            emit(result)
+        }
+    }.catch { exception ->
+        emit(Result.failure(exception))
+    }
 
     fun updateUser(user: User): Flow<Result<String>> = flow {
         withContext(Dispatchers.IO) {
             userDao.updateUser(user)
         }
-        emit(Result.success("User updated successfully"))
+        firebaseDataSource.updateUser(user).collect { result ->
+            emit(result)
+        }
     }.catch { exception ->
         emit(Result.failure(exception))
     }
@@ -46,7 +72,9 @@ class UserRepository(private val userDao: UserDao) {
         withContext(Dispatchers.IO) {
             userDao.deleteUserById(id)
         }
-        emit(Result.success("User deleted successfully"))
+        firebaseDataSource.deleteUserById(id).collect { result ->
+            emit(result)
+        }
     }.catch { exception ->
         emit(Result.failure(exception))
     }
