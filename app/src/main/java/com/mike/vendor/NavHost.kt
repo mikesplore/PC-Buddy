208b089e76a625808ab71f68b90c621f3f3dcf82
mikesplore <mikepremium8@gmail.com>
2025-01-27 12:39:55 +0300

    Basic functions implemented

--- app/src/main/java/com/mike/vendor/NavHost.kt
+++ app/src/main/java/com/mike/vendor/NavHost.kt
@@ -1,10 +1,29 @@
 package com.mike.vendor
 
+import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mike.vendor.deviceControl.ServerControlScreen
 
 @Composable
 fun AppNavHost(
-
+    context: Context
 ) {
+    val navController = rememberNavController()
 
+    NavHost(startDestination = "home", navController = navController) {
+        composable("home") {
+            AvailableDevicesScreen(navController)
+        }
+
+        composable("device/{macAddress}") { backStackEntry ->
+            ServerControlScreen(
+                macAddress = backStackEntry.arguments?.getString("macAddress") ?: "",
+                context = context,
+                navController = navController
+            )
+        }
+    }
 }
