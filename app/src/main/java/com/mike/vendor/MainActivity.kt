d3fc5e363262d131007444b69623e512bd80afd9
mikesplore <mikepremium8@gmail.com>
2025-01-27 12:40:04 +0300

    Basic functions implemented

--- app/src/main/java/com/mike/vendor/MainActivity.kt
+++ app/src/main/java/com/mike/vendor/MainActivity.kt
@@ -1,42 +1,41 @@
 package com.mike.vendor
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import com.mike.vendor.model.Server
+import com.mike.vendor.model.dao.ServerDao
+import com.mike.vendor.networkManager.fetchServers
 import com.mike.vendor.ui.theme.VendorTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var serverDao: ServerDao
+
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             enableEdgeToEdge()
-            VendorTheme {
-                ShowDevices(this)
+            remember { mutableStateListOf<Server>() }
+            VendorTheme(darkTheme = true) {
+                val scope = rememberCoroutineScope()
+                remember { mutableStateListOf<Server>() }
+                LaunchedEffect(Unit) {
+                    fetchServers(serverDao, scope)
+                }
+                AppNavHost(this)
             }
         }
     }
 }
 
-@Composable
-fun ShowDevices(context: Context){
-    val devices = remember { mutableStateListOf<DeviceInfo>() }
-    LaunchedEffect(Unit) {
-        devices.addAll(getConnectedDevices(context))
-    }
-    LazyColumn {
-        itemsIndexed(devices) { index, device ->
-            Text(text = "Device $index: IP - ${device.ipAddress}, Host - ${device.hostName}")
-        }
-    }
-}
