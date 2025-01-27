6a39d5d1f5cccd83b63f023017c81f7b7b69a76b
mikesplore <mikepremium8@gmail.com>
2025-01-27 12:41:12 +0300

    Basic functions implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,11 @@ dependencies {
     //navigation compose
     implementation("androidx.navigation:navigation-compose:2.8.5")
 
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+
     //coil dependency
     implementation(libs.coil.compose)
 
