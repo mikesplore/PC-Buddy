5e2159c4b24097dddf26bbfc94547e678a5445c8
mikesplore <mikepremium8@gmail.com>
2025-01-27 12:39:39 +0300

    Basic functions implemented

--- app/src/main/java/com/mike/vendor/model/NetworkDevice.kt
+++ app/src/main/java/com/mike/vendor/model/NetworkDevice.kt
@@ -1,17 +1,15 @@
 package com.mike.vendor.model
 
-import android.net.nsd.NsdServiceInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "network_devices")
-data class NetworkDevice(
+@Entity(tableName = "servers")
+data class Server(
     @PrimaryKey val macAddress: String,
     val name: String,
     val host: String,
     val port: Int,
-    val serviceInfo: NsdServiceInfo,
-    val onlineStatus: Boolean
+    var onlineStatus: Boolean = true
 ){
-    constructor(macAddress: String, name: String, host: String, port: Int, onlineStatus: Boolean) : this(macAddress, name, host, port, NsdServiceInfo(), onlineStatus)
+    constructor(): this("", "", "", 0)
 }
