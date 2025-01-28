566b28b1aca07a2d332f80c5596532c965e7feda
mikesplore <mikepremium8@gmail.com>
2025-01-28 10:04:08 +0300

    Basic functions implemented

--- app/src/main/java/com/mike/vendor/model/Database.kt
+++ app/src/main/java/com/mike/vendor/model/Database.kt
@@ -3,15 +3,25 @@ package com.mike.vendor.model
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.mike.vendor.model.dao.BatteryDao
+import com.mike.vendor.model.dao.MemoryAndStorageDao
 import com.mike.vendor.model.dao.ServerDao
+import com.mike.vendor.model.dataClasses.BatteryDetails
+import com.mike.vendor.model.dataClasses.MemoryDetails
+import com.mike.vendor.model.dataClasses.StorageInfo
 
 
 @Database(
     entities = [
-       Server::class
+       Server::class,
+       BatteryDetails::class,
+         MemoryDetails::class,
+            StorageInfo::class,
     ], version = 1, exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun serverDao(): ServerDao
+    abstract fun batteryDao(): BatteryDao
+    abstract fun memoryAndStorageDao(): MemoryAndStorageDao
 }
