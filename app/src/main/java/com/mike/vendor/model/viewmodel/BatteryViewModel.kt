a6d51a67b45809b125ce9a00767f52b77fc6ec4e
mikesplore <mikepremium8@gmail.com>
2025-01-28 10:04:50 +0300

    Basic functions implemented

---
+++ app/src/main/java/com/mike/vendor/model/viewmodel/BatteryViewModel.kt
@@ -0,0 +1,49 @@
+package com.mike.vendor.model.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mike.vendor.model.dataClasses.BatteryDetails
+import com.mike.vendor.model.repositories.BatteryRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BatteryViewModel @Inject constructor(private val batteryRepository: BatteryRepository) :
+    ViewModel() {
+    private val _batteryDetails = MutableStateFlow<BatteryDetails?>(null)
+    val batteryDetails = _batteryDetails.asStateFlow()
+
+    fun getBatteryDetails(macAddress: String) {
+        Log.d("BatteryViewModel", "Fetching battery details for $macAddress")
+        viewModelScope.launch(Dispatchers.Main) {
+            batteryRepository.getBatteryDetails(macAddress).collect { batteryDetails ->
+                Log.d("BatteryViewModel", "Battery details fetched: $batteryDetails")
+                _batteryDetails.value = batteryDetails
+            }
+        }
+    }
+
+    fun insertBatteryDetails(batteryDetails: BatteryDetails) {
+        viewModelScope.launch(Dispatchers.IO) {
+            batteryRepository.insertBatteryDetails(batteryDetails)
+        }
+    }
+
+    fun updateBatteryDetails(batteryDetails: BatteryDetails) {
+        viewModelScope.launch(Dispatchers.IO) {
+            batteryRepository.insertBatteryDetails(batteryDetails)
+        }
+    }
+
+    fun deleteAllBatteryDetails() {
+        viewModelScope.launch(Dispatchers.IO) {
+            batteryRepository.deleteAllBatteryDetails()
+        }
+    }
+
+}
