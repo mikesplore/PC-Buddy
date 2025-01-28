63a801846004d145f8a7055a2d0de6cdb76bdac0
mikesplore <mikepremium8@gmail.com>
2025-01-28 10:03:18 +0300

    Basic functions implemented

--- app/src/main/java/com/mike/vendor/NavHost.kt
+++ app/src/main/java/com/mike/vendor/NavHost.kt
@@ -5,7 +5,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.mike.vendor.deviceControl.AvailableDevicesScreen
 import com.mike.vendor.deviceControl.ServerControlScreen
+import com.mike.vendor.specs.battery.BatteryDetailsScreen
 
 @Composable
 fun AppNavHost(
@@ -23,6 +25,14 @@ fun AppNavHost(
                 macAddress = backStackEntry.arguments?.getString("macAddress") ?: "",
                 context = context,
                 navController = navController
+            )
+        }
+
+        composable("battery/{macAddress}") { backStackEntry ->
+            BatteryDetailsScreen(
+                macAddress = backStackEntry.arguments?.getString("macAddress") ?: "",
+                navController = navController,
+                context = context
             )
         }
     }
