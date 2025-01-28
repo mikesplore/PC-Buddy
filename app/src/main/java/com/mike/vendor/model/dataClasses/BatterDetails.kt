ca269801dd691de2935546160711a99d1d55177e
mikesplore <mikepremium8@gmail.com>
2025-01-28 10:04:54 +0300

    Basic functions implemented

---
+++ app/src/main/java/com/mike/vendor/model/dataClasses/BatterDetails.kt
@@ -0,0 +1,25 @@
+package com.mike.vendor.model.dataClasses
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "battery")
+data class BatteryDetails(
+    @PrimaryKey val macAddress: String,
+    val name: String,
+    val remainingCapacity: Double,
+    val timeRemaining: Long,
+    val voltage: Double,
+    val temperature: Double,
+    val isCharging: Boolean,
+    val cycleCount: Int,
+    val designCapacity: Int,
+    val fullChargeCapacity: Int,
+    val batteryHealth: Double,
+    val batteryChemistry: String,
+    val batteryStatus: String,
+    val manufactureDate: String,
+    val serialNumber: String
+){
+    constructor(): this("", "", 0.0, 0, 0.0, 0.0, false, 0, 0, 0, 0.0, "", "", "", "")
+}
