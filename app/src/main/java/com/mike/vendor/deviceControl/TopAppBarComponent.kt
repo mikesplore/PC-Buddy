c1e6f21f780f63ce43234c2c27655d1598517d06
mikesplore <mikepremium8@gmail.com>
2025-01-28 10:02:57 +0300

    Basic functions implemented

--- app/src/main/java/com/mike/vendor/deviceControl/TopAppBarComponent.kt
+++ app/src/main/java/com/mike/vendor/deviceControl/TopAppBarComponent.kt
@@ -1,10 +1,13 @@
 package com.mike.vendor.deviceControl
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBackIos
 import androidx.compose.material.icons.filled.Computer
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -12,11 +15,14 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.mike.vendor.ui.theme.CommonComponents as CC
+import androidx.compose.runtime.getValue
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
@@ -25,11 +31,15 @@ fun TopAppBarComponent(
     onlineStatus: Boolean,
     onBackPressed: () -> Unit
 ) {
+    val tintcolor by animateColorAsState(
+        targetValue = if (onlineStatus) Color.Green else Color.Red,
+        tween(500)
+    )
     TopAppBar(
         navigationIcon = {
             IconButton(onClick = onBackPressed) {
                 Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBackIos,
                     contentDescription = "Back"
                 )
             }
@@ -39,7 +49,8 @@ fun TopAppBarComponent(
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.padding(horizontal = 8.dp)
             ) {
-                Icon(Icons.Default.Computer, null, tint = if (onlineStatus) Color.Green else Color.Red,
+
+                Icon(Icons.Default.Computer, null, tint = tintcolor,
                     modifier = Modifier.padding(8.dp))
                 Text(
                     text = deviceName,
@@ -48,6 +59,9 @@ fun TopAppBarComponent(
                     modifier = Modifier.padding(8.dp),
                 )
             }
-        }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = CC.secondary().copy(0.9f),
+        )
     )
 }
