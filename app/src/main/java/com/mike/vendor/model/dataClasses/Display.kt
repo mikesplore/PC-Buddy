d9e9ee9e868f362809f24b39364d809ce4b671db
mikesplore <mikepremium8@gmail.com>
2025-01-28 16:01:03 +0300

    Storage and Memory functions implemented

---
+++ app/src/main/java/com/mike/vendor/model/dataClasses/Display.kt
@@ -0,0 +1,15 @@
+package com.mike.vendor.model.dataClasses
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "display_info")
+data class DisplayInfo(
+    @PrimaryKey val macAddress: String,
+    val deviceName: String,
+    val width: Int,
+    val height: Int,
+    val refreshRate: Int
+){
+    constructor(macAddress: String, deviceName: String) : this(macAddress, deviceName, 0, 0, 0)
+}
