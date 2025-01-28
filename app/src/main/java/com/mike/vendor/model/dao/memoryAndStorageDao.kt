b0b401e3d6bfe4b17e54dce8dee8c2a67f78ecf9
mikesplore <mikepremium8@gmail.com>
2025-01-28 10:03:26 +0300

    Basic functions implemented

---
+++ app/src/main/java/com/mike/vendor/model/dao/memoryAndStorageDao.kt
@@ -0,0 +1,25 @@
+package com.mike.vendor.model.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.mike.vendor.model.dataClasses.MemoryDetails
+import com.mike.vendor.model.dataClasses.StorageInfo
+
+@Dao
+interface MemoryAndStorageDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMemoryDetails(memoryDetails: MemoryDetails)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertStorageInfo(storageInfo: StorageInfo)
+
+    @Query("SELECT * FROM memory_details WHERE macAddress = :macAddress")
+    suspend fun getMemoryDetails(macAddress: String): MemoryDetails
+
+    @Query("SELECT * FROM storage_info WHERE macAddress = :macAddress")
+    suspend fun getStorageInfo(macAddress: String): StorageInfo
+
+}
+
