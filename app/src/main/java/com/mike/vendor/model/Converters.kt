4308c0cb40c57fa946f6cfe23a6c726e151f5f1f
mikesplore <mikepremium8@gmail.com>
2025-01-28 10:04:17 +0300

    Basic functions implemented

--- app/src/main/java/com/mike/vendor/model/Converters.kt
+++ app/src/main/java/com/mike/vendor/model/Converters.kt
@@ -1,38 +1,23 @@
 package com.mike.vendor.model
 
-import android.net.nsd.NsdServiceInfo
 import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.mike.vendor.model.dataClasses.MountPointDetails
 
 class Converters {
     @TypeConverter
-    fun fromServiceInfo(serviceInfo: NsdServiceInfo): String {
-        return serviceInfo.toString()
+    fun fromMountPointDetailsList(value: List<MountPointDetails>): String {
+        val gson = Gson()
+        val type = object : TypeToken<List<MountPointDetails>>() {}.type
+        return gson.toJson(value, type)
     }
 
     @TypeConverter
-    fun toServiceInfo(serviceInfoString: String): NsdServiceInfo {
-        // Implement the conversion logic from String to NsdServiceInfo
-        return NsdServiceInfo()
+    fun toMountPointDetailsList(value: String): List<MountPointDetails> {
+        val gson = Gson()
+        val type = object : TypeToken<List<MountPointDetails>>() {}.type
+        return gson.fromJson(value, type)
     }
-
-    fun fromList(list: List<String>): String {
-        return list.joinToString(",")
-    }
-
-    fun toList(listString: String): List<String> {
-        return listString.split(",")
-    }
-
-    fun fromMap(map: Map<String, String>): String {
-        return map.map { "${it.key}:${it.value}" }.joinToString(",")
-    }
-
-    fun toMap(mapString: String): Map<String, String> {
-        return mapString.split(",").associate {
-            val (key, value) = it.split(":")
-            key to value
-        }
-    }
-
 
 }
