844a277ddba75a7bdeace3d6e3a557e29e269782
mikesplore <mikepremium8@gmail.com>
2025-01-30 22:02:37 +0300

    update

--- app/src/main/java/com/mike/vendor/api/Commands Api Service.kt
+++ app/src/main/java/com/mike/vendor/api/Commands Api Service.kt
@@ -1,10 +1,16 @@
 package com.mike.vendor.api
 
 
+import com.mike.vendor.model.dao.SystemInfoDao
 import com.mike.vendor.model.dataClasses.BatteryDetails
+import com.mike.vendor.model.dataClasses.ComputerSystemDetails
 import com.mike.vendor.model.dataClasses.DisplayInfo
 import com.mike.vendor.model.dataClasses.MemoryDetails
+import com.mike.vendor.model.dataClasses.OperatingSystemInfo
+import com.mike.vendor.model.dataClasses.ProcessorDetails
+import com.mike.vendor.model.dataClasses.SoftwareInfo
 import com.mike.vendor.model.dataClasses.StorageInfo
+import com.mike.vendor.model.dataClasses.UserEnvironmentInfo
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -39,6 +45,23 @@ interface CommandApiService {
 
     @GET("displays")
     fun getDisplay(): Call<List<DisplayInfo>>
+
+
+    @GET("softwareInfo")
+    fun getSoftware(): Call<SoftwareInfo>
+
+    @GET("processorDetails")
+    fun getProcessor(): Call<ProcessorDetails>
+
+    @GET("operatingSystemInfo")
+    fun getOperatingSystemInfo(): Call<OperatingSystemInfo>
+
+    @GET("userEnvironmentInfo")
+    fun getUserEnvironmentInfo(): Call<UserEnvironmentInfo>
+
+    @GET("systemInfo")
+    fun getCurrentSystemInfo(): Call<ComputerSystemDetails>
+
 
 }
 
