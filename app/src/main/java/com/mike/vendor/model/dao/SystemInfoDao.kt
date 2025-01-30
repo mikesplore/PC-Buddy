e3dc1711e32d4be4f5737c75d0a1c2dd6d4ec3b5
mikesplore <mikepremium8@gmail.com>
2025-01-30 22:02:15 +0300

    update

---
+++ app/src/main/java/com/mike/vendor/model/dao/SystemInfoDao.kt
@@ -0,0 +1,55 @@
+package com.mike.vendor.model.dao
+
+import androidx.room.*
+import com.mike.vendor.model.dataClasses.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SystemInfoDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertProcessorDetails(processorDetails: ProcessorDetails)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertComputerSystemDetails(computerSystemDetails: ComputerSystemDetails)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOperatingSystemInfo(operatingSystemInfo: OperatingSystemInfo)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSoftwareInfo(softwareInfo: SoftwareInfo)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUserEnvironmentInfo(userEnvironmentInfo: UserEnvironmentInfo)
+
+    @Transaction
+    suspend fun insertAllSystemInfo(
+        processorDetails: ProcessorDetails,
+        computerSystemDetails: ComputerSystemDetails,
+        operatingSystemInfo: OperatingSystemInfo,
+        softwareInfo: SoftwareInfo,
+        userEnvironmentInfo: UserEnvironmentInfo
+    ) {
+        insertProcessorDetails(processorDetails)
+        insertComputerSystemDetails(computerSystemDetails)
+        insertOperatingSystemInfo(operatingSystemInfo)
+        insertSoftwareInfo(softwareInfo)
+        insertUserEnvironmentInfo(userEnvironmentInfo)
+    }
+
+    @Query("SELECT * FROM processor_details WHERE macAddress = :macAddress")
+    fun getProcessorDetails(macAddress: String): Flow<ProcessorDetails>
+
+    @Query("SELECT * FROM computer_system_details WHERE macAddress = :macAddress")
+    fun getComputerSystemDetails(macAddress: String): Flow<ComputerSystemDetails>
+
+    @Query("SELECT * FROM operating_system_info WHERE macAddress = :macAddress")
+    fun getOperatingSystemInfo(macAddress: String): Flow<OperatingSystemInfo>
+
+    @Query("SELECT * FROM software_info WHERE macAddress = :macAddress")
+    fun getSoftwareInfo(macAddress: String): Flow<SoftwareInfo>
+
+    @Query("SELECT * FROM user_environment_info WHERE macAddress = :macAddress")
+    fun getUserEnvironmentInfo(macAddress: String): Flow<UserEnvironmentInfo>
+
+}
