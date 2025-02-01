796b3511e1584434bdfc9477c3a2f0144899527e
mikesplore <mikepremium8@gmail.com>
2025-02-01 09:26:06 +0300

    update

--- app/src/main/java/com/mike/vendor/model/repositories/DisplayRepository.kt
+++ app/src/main/java/com/mike/vendor/model/repositories/DisplayRepository.kt
@@ -7,6 +7,6 @@ import javax.inject.Inject
 class DisplayRepository @Inject constructor(private val displayDao: DisplayDao){
     fun getDisplay(macAddress: String) = displayDao.getDisplayInfo(macAddress)
      fun insertDisplay(display: DisplayInfo) = displayDao.insertDisplayInfo(display)
-     fun deleteDisplay() = displayDao.deleteDisplayInfo()
+     fun deleteDisplay(macAddress: String) = displayDao.deleteDisplayInfo(macAddress)
 
 }
