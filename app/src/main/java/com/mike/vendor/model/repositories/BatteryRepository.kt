1dac44cd9127b5e7560ef601224ec22c4b0dfe77
mikesplore <mikepremium8@gmail.com>
2025-02-01 09:26:25 +0300

    update

--- app/src/main/java/com/mike/vendor/model/repositories/BatteryRepository.kt
+++ app/src/main/java/com/mike/vendor/model/repositories/BatteryRepository.kt
@@ -7,6 +7,6 @@ import javax.inject.Inject
 class BatteryRepository @Inject constructor(private val batteryDao: BatteryDao) {
      fun getBatteryDetails(macAddress: String) = batteryDao.getBatteryDetails(macAddress)
     suspend fun insertBatteryDetails(batteryDetails: BatteryDetails) = batteryDao.insertBatteryDetails(batteryDetails)
-    suspend fun deleteAllBatteryDetails() = batteryDao.deleteAllBatteryDetails()
+    suspend fun deleteAllBatteryDetails(macAddress: String) = batteryDao.deleteAllBatteryDetails(macAddress)
 
 }
