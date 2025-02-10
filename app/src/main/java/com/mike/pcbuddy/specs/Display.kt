826993ebb37cbebf5b9c548bbb4f21a6bad93fc8
mikesplore <mikepremium8@gmail.com>
2025-02-10 10:39:55 +0300

    Done

--- app/src/main/java/com/mike/pcbuddy/specs/Display.kt
+++ app/src/main/java/com/mike/pcbuddy/specs/Display.kt
@@ -1,4 +1,4 @@
-package com.mike.vendor.specs
+package com.mike.pcbuddy.specs
 
 import android.content.Context
 import android.util.Log
@@ -8,10 +8,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,9 +17,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -45,24 +40,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
-import androidx.compose.ui.input.nestedscroll.NestedScrollSource
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.min
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.mike.vendor.R
-import com.mike.vendor.api.fetchDisplayInfo
-import com.mike.vendor.model.dataClasses.DisplayInfo
-import com.mike.vendor.model.viewmodel.DisplayViewModel
-import com.mike.vendor.model.viewmodel.ServerViewModel
-import com.mike.vendor.ui.theme.CommonComponents as CC
+import com.mike.pcbuddy.R
+import com.mike.pcbuddy.api.fetchDisplayInfo
+import com.mike.pcbuddy.model.dataClasses.DisplayInfo
+import com.mike.pcbuddy.model.viewmodel.DisplayViewModel
+import com.mike.pcbuddy.model.viewmodel.ServerViewModel
+import com.mike.pcbuddy.ui.theme.CommonComponents as CC
 
 @Composable
 fun DisplayInfoScreen(
