3ac1fe7d150218ef2f30122c5485b24cba624835
mikesplore <mikepremium8@gmail.com>
2025-02-10 10:39:45 +0300

    Done

--- app/src/androidTest/java/com/mike/pcbuddy/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mike/pcbuddy/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.mike.vendor
+package com.mike.pcbuddy
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mike.vendor", appContext.packageName)
+        assertEquals("com.mike.pcbuddy", appContext.packageName)
     }
 }
