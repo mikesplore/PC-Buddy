9a321a4e2a9f772b24c25af739cae0fe0b7560f3
mikesplore <mikepremium8@gmail.com>
2025-02-10 10:38:57 +0300

    Done

--- app/src/main/java/com/mike/pcbuddy/model/dao/SystemInfoDao.kt
+++ app/src/main/java/com/mike/pcbuddy/model/dao/SystemInfoDao.kt
@@ -1,7 +1,7 @@
-package com.mike.vendor.model.dao
+package com.mike.pcbuddy.model.dao
 
 import androidx.room.*
-import com.mike.vendor.model.dataClasses.*
+import com.mike.pcbuddy.model.dataClasses.*
 import kotlinx.coroutines.flow.Flow
 
 @Dao
