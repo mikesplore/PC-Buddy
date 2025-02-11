051641e2348cdf3ebb89c83a32ddd85e18097d27
mikesplore <mikepremium8@gmail.com>
2025-02-11 16:36:56 +0300

    Done

--- app/src/main/java/com/mike/pcbuddy/MainActivity.kt
+++ app/src/main/java/com/mike/pcbuddy/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.mike.pcbuddy
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import com.mike.pcbuddy.networkManager.fetchServers
 import com.mike.pcbuddy.ui.theme.PcBuddyTheme
 import com.mike.pcbuddy.model.dao.ServerDao
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             enableEdgeToEdge()
 
-            PcBuddyTheme(darkTheme = true) {
+            PcBuddyTheme(darkTheme = true, dynamicColor = false) {
                 val scope = rememberCoroutineScope()
                 LaunchedEffect(Unit) {
                     fetchServers(serverDao, scope)
@@ -32,3 +38,11 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
+
+@Composable
+fun ActionButton(conntext: Context) {
+    AppNavHost(conntext)
+}
+
