cd6e39532746c7710285fb60139abc15172282e9
mikesplore <mikepremium8@gmail.com>
2025-02-11 16:36:38 +0300

    Done

--- app/src/main/java/com/mike/pcbuddy/ui/theme/Color.kt
+++ app/src/main/java/com/mike/pcbuddy/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package com.mike.pcbuddy.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val primaryColor = Color(0xff1A3636)
-val secondaryColor = Color(0xff40534C)
-val tertiaryColor = Color(0xff677D6A)
-val textColor = Color(0xffD6BD98)
+val primaryColor = Color(0xff3674B5)
+val secondaryColor = Color(0xff578FCA)
+val tertiaryColor = Color(0xffA1E3F9)
+val textColor = Color(0xffD1F8EF)
 val extraColor = Color(0xff3C3D37)
